69e0b2efa69512bcf620b4e86232ab715d5f7ccc
nickkept <nickit1899@gmail.com>
2022-11-10 18:18:44 +0100

    reset

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,5 @@
 const btnGenerateGrid = document.getElementById(`btn_generate_grid`);
 const select = document.querySelector("select");
-let bombs;
 btnGenerateGrid.addEventListener(`click`, function () {
     // console.log(this);
     // const diffSelect = document.querySelector(`#diff-select`) ;
@@ -14,7 +13,6 @@ btnGenerateGrid.addEventListener(`click`, function () {
     }
     const difficulty = select.value;
     // console.log(difficulty);
-    bombs = generateBombsList( difficulty );
     generateGrid(difficulty);
     // console.log(numCells.value);
 });
@@ -31,37 +29,10 @@ function generateGrid(difficulty) {
         newCell.style.width = `calc(100% / ${difficulty})`;
         newCell.textContent = i + 1;
         newCell.dataset.numCella = i + 1;
-        console.log(newCell.dataset.numCella);
-        newCell.addEventListener("click", onCellClick);
-        // newCell.innerHTML = generateRandomNumb( 1, totalCells );
-        // newCell.addEventListener(`click`, function (onCellClick) {
-        //     // this.classList.toggle("bg-success");
-        //     console.log(i)
-        // });
+        newCell.addEventListener(`click`, function () {
+            this.classList.toggle("bg-success");
+            console.log(i)
+        });
         gridContainer.append(newCell);
-    }
-}
-function onCellClick() {
-    const numCella = +this.dataset.numCella;
-
-    if (bombs.includes(numCella)) {
-        alert("Hai trovato una bomba!!! Game Over!");
-    }
-    function generateRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    function generateBombsList(totalCells) {
-        const bombsList = [];
-
-        while (bombsList.length < 16) {
-            // random numb
-            const num = generateRandomNumber(1, totalCells);
-            if (!bombsList.includes(num)) {
-                bombsList.push(num);
-            }
-        }
-
-        return bombsList;
     }
 }
