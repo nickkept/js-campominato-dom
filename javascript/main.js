dafb7f8a5cb3e363c7289679f2e9e17495f6088b
nickkept <nickit1899@gmail.com>
2022-11-17 11:22:52 +0100

    finished project

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,55 @@
-const btnGenerateGrid = document.getElementById(`btn_generate_grid`);
-const select = document.querySelector("select");
-btnGenerateGrid.addEventListener(`click`, function () {
-    // console.log(this);
-    // const diffSelect = document.querySelector(`#diff-select`) ;
-    // console.log(select);
-    // console.log(numCells);
-    // const numCells = document.getElementById(`num-cells`);
-    if (select.value == 7) {
-        alert(`Are you sure?`)
-    } else if (select.value == 10) {
-        alert(`Don't be a pu**y`)
-    }
-    const difficulty = select.value;
-    // console.log(difficulty);
-    generateGrid(difficulty);
-    // console.log(numCells.value);
+const btnGenerate = document.getElementById("btn_generate_grid");
+const selectLevel = document.querySelector("[name='select-level']");
+let bombs;
+
+btnGenerate.addEventListener("click", function () {
+    const difficulty = selectLevel.value;
+    bombs = generateBombsList(+difficulty);
+    generateCells(+difficulty);
 });
 
-function generateGrid(difficulty) {
-    const gridContainer = document.querySelector(`.grid-container`);
-    gridContainer.innerHTML = ``;
-    const totalCells = difficulty * difficulty;
+function generateCells(numCelle) {
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.classList.add("d-none");
+    gridContainer.innerHTML = "";
 
-    for (let i = 0; i < totalCells; i++) {
-        const newCell = document.createElement(`div`);
-        newCell.classList.add(`grid-cell`);
-        newCell.style.flexBasis = `calc(100% / ${difficulty})`;
-        newCell.style.width = `calc(100% / ${difficulty})`;
+    for (let i = 0; i < numCelle; i++) {
+        const newCell = document.createElement("div");
+        const rowsNum = Math.sqrt(numCelle);
+        newCell.classList.add("cell");
+        newCell.style.width = 100 / rowsNum + "%";
         newCell.textContent = i + 1;
         newCell.dataset.numCella = i + 1;
-        newCell.addEventListener(`click`, function () {
-            this.classList.toggle("bg-success");
-            console.log(i)
-        });
+        newCell.addEventListener("click", onCellClick);
         gridContainer.append(newCell);
     }
+
+    gridContainer.classList.remove("d-none");
+}
+
+function onCellClick() {
+    const numCella = +this.dataset.numCella;
+
+    if (bombs.includes(numCella)) {
+        alert("Hai beccato una bomba! RIP!");
+        this.classList.add("active");
+    } else {
+        this.classList.toggle("active");
+    }
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateBombsList(numCelle) {
+    const bombsList = [];
+    while (bombsList.length < 16) {
+        const num = generateRandomNumber(1, numCelle);
+        if (!bombsList.includes(num)) {
+            bombsList.push(num);
+        }
+    }
+
+    return bombsList;
 }
